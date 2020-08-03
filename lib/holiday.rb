82085a1e0bdaafb8cc2939bd1692d83086f0f9fc
Karim Alomari <karim.alomari@gmail.com>
2020-08-03 15:03:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -5,7 +5,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter].each do |holiday, value|
+  holiday_hash[:winter].each do |holiday, values|
     value << supply
   end
 end
